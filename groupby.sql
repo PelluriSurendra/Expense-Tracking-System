2204479785ecedad83fb406ae288a8d8cbec7866
PelluriSurendra <39879820+PelluriSurendra@users.noreply.github.com>
2025-06-09 11:43:03 +0530

    Delete groupby.sql

--- groupby.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-            l.name, 
-            COUNT(m.movie_id) as no_movies
-	FROM languages l
-	LEFT JOIN movies m USING (language_id)        
-	GROUP BY language_id
-	ORDER BY no_movies DESC;
