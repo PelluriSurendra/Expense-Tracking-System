23dfeff6fc4ca1820a34c801291444bdccd3afc8
PelluriSurendra <39879820+PelluriSurendra@users.noreply.github.com>
2025-06-09 11:41:48 +0530

    Delete cpncat.sql

--- cpncat.sql
+++
@@ -1,2 +0,0 @@
-select m.title,group_concat(a.name separator " | ") as actors from movies m join movie_actor ma on m.movie_id = ma.movie_id 
-join actors a on a.actor_id = ma.actor_id group by m.movie_id
