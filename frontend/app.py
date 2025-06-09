a5c0dcc0d44e75a194bb578d3a085120cd6721ef
PelluriSurendra <39879820+PelluriSurendra@users.noreply.github.com>
2025-06-09 16:01:58 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -2,11 +2,13 @@ import streamlit as st
 from add_update_ui import add_update_tab
 from analytics_ui import analytics_tab
 from budget_ui import budget_tab
+from analytics_by_month import analytics_months_tab
+
 
 
 st.title("Expense Tracking System")
 
-tab1, tab2,tab3 = st.tabs(["Add/Update", "Analytics","Budget"])
+tab1, tab2,tab3,tab4 = st.tabs(["Add/Update", "Analytics","Budget","Analytics By Months"])
 
 with tab1:
     add_update_tab()
@@ -16,4 +18,8 @@ with tab2:
 
 with tab3:
     budget_tab()
+
+with tab4:
+    analytics_months_tab()
+
 
